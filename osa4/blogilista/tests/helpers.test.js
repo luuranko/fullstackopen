a70a55aef07dde34d5defd6cd5e27e9afb6060bb
Lauri Halttunen <lauri.halttunen@helsinki.fi>
2024-02-22 10:36:46 +0200

    4a

---
+++ osa4/blogilista/tests/helpers.test.js
@@ -0,0 +1,81 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const
+  { dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes, } = require('../utils/helper_functions')
+const { blogList } = require('./testing_material')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [blogList[1]] // djikstra, likes 5
+  const listWithThreeBlogs = blogList.slice(0, 3) // likes 24
+
+  test('of empty list is zero', () => {
+    assert.strictEqual(totalLikes([]), 0)
+  })
+  test('when list has only one blog equals the likes of that', () => {
+    const result = totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  test('of a bigger list is calculated correctly', () => {
+    assert.strictEqual(totalLikes(listWithThreeBlogs), 24)
+    assert.strictEqual(totalLikes(blogList), 36)
+  })
+})
+
+describe('favoriteBlog', () => {
+  const listWithOneBlog = [blogList[1]] // djikstra, likes 5
+  const listWithThreeBlogs = blogList.slice(3)
+
+  test('when list has only one blog equals that blog', () => {
+    assert.strictEqual(favoriteBlog(listWithOneBlog).likes, 5)
+  })
+  test('is calculated correctly', () => {
+    assert.strictEqual(favoriteBlog(listWithThreeBlogs).likes, 10)
+    assert.strictEqual(favoriteBlog(blogList).likes, 12)
+  })
+  test('is undefined when list is empty', () => {
+    assert.strictEqual(favoriteBlog([]), undefined)
+  })
+})
+
+describe('mostBlogs', () => {
+  const listWithOneBlog = [blogList[0]]
+  const listWithFourBlogs = blogList.slice(0, 4)
+
+  test('when list has only one blog has an author with blog count 1', () => {
+    assert.strictEqual(mostBlogs(listWithOneBlog).blogs, 1)
+  })
+  test('of a bigger list is calculated correctly', () => {
+    assert.strictEqual(mostBlogs(listWithFourBlogs).blogs, 2)
+    assert.strictEqual(mostBlogs(blogList).blogs, 3)
+  })
+  test('is undefined when list is empty', () => {
+    assert.strictEqual(mostBlogs([]), undefined)
+  })
+})
+
+describe('mostLikes', () => {
+  const listWithOneBlog = [blogList[0]]
+  const listWithFourBlogs = blogList.slice(0, 4)
+
+  test('when list has only one blog has an author with the like count of that blog', () => {
+    assert.strictEqual(mostLikes(listWithOneBlog).likes, 7)
+  })
+  test('of a bigger list is calculated correctly', () => {
+    assert.strictEqual(mostLikes(listWithFourBlogs).likes, 17)
+    assert.strictEqual(mostLikes(blogList).likes, 17)
+  })
+  test('is undefined when list is empty', () => {
+    assert.strictEqual(mostLikes([]), undefined)
+  })
+})
--- osa4/blogilista/utils/helper_functions.js
+++ osa4/blogilista/utils/helper_functions.js
@@ -1,18 +1,45 @@
-const reverse = (string) => {
-  return string
-    .split('')
-    .reverse()
-    .join('')
+const dummy = (blogs) => {
+  return 1
 }
 
-const average = (array) => {
-  const reducer = (sum, item) => {
-    return sum + item
-  }
-  return array.reduce(reducer, 0) / array.length
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.slice().sort((a,b) => b.likes - a.likes)[0]
+}
+
+const mostBlogs = (blogs) => {
+  const blogsByAuthor = []
+  blogs.forEach((blog) => {
+    const authorAdded = blogsByAuthor.find((el) => el.author === blog.author)
+    if (authorAdded) {
+      authorAdded.blogs += 1
+    } else {
+      blogsByAuthor.push({ author: blog.author, blogs: 1 })
+    }
+  })
+  return blogsByAuthor.slice().sort((a,b) => b.blogs - a.blogs)[0]
+}
+
+const mostLikes = (blogs) => {
+  const blogsByAuthor = []
+  blogs.forEach((blog) => {
+    const authorAdded = blogsByAuthor.find((el) => el.author === blog.author)
+    if (authorAdded) {
+      authorAdded.likes += blog.likes
+    } else {
+      blogsByAuthor.push({ author: blog.author, likes: blog.likes })
+    }
+  })
+  return blogsByAuthor.slice().sort((a,b) => b.likes - a.likes)[0]
 }
 
 module.exports = {
-  reverse,
-  average,
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
