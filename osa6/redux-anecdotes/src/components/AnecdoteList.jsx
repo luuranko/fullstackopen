072888c066e1f900dd126526ee861eb500191f1c
Lauri Halttunen <lauri.halttunen@helsinki.fi>
2024-09-07 21:39:05 +0300

    6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,8 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteId } from '../reducers/anecdoteReducer'
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
+  const anecdotes = useSelector(state => {
+    if (state.filter) {
+      return state.anecdotes.filter(a => a.content.includes(state.filter))
+    }
+    return state.anecdotes
+  })
 
   const vote = id => {
     dispatch(voteId(id))
